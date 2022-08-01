81931a98d5738eeb77108dafdbbac75c188534fa
Sina <sinarahimi.main@example.com>
2022-08-01 20:36:41 +0430

    Can produce the QRCode, but it disappears immidietly

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import { QRCode } from 'qrcode';
+import QRCode from 'qrcode';
 
 function App() {
 
-  const [url, setUrl] = useState(null);
-  const [qrcode, setQrCode] = useState(null);
+  const [src, setSrc] = useState(null);
+  const [qrcode, setQRCode] = useState(null);
 
   const GenerateQRCode = () => {
-    QRCode.(url, (err, url) => {
-      if (err) return console.error(err)
-
-      console.log(url);
-      setQrCode(url);
+    QRCode.toDataURL(src).then((data) => {
+      setQRCode(data);
     })
   }
 
@@ -21,18 +18,19 @@ function App() {
   return (
     <div className="App">
 
-        <div className=".wrapper">
+        <div>
 
           <h1>QRCode Generator</h1>
           <p>Enter your contract address here</p>
 
           <form>
-            <input type="text" placeholder="Put contract address here!" value={url} onChange={(x) => setUrl(x.target.value)}></input>
+            <input type="text" placeholder="Put contract address here!" onChange={(x) => setSrc(x.target.value)}></input>
             <button onClick={GenerateQRCode}>Generate QR Code</button>
           </form>
 
-          <img src={qrcode} />
         </div>
+
+        <img src={qrcode}/>
 
     </div>
   );
