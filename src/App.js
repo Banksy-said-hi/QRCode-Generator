10198ee39b2d9dab22a1dcdc3c4fa27cd3d82b49
Sina <sinarahimi.main@example.com>
2022-08-01 13:55:49 +0430

    Made some advancements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { QRCode } from 'qrcode';
 
 function App() {
+
+  const [url, setUrl] = useState(null);
+  const [qrcode, setQrCode] = useState(null);
+
+  const GenerateQRCode = () => {
+    QRCode.(url, (err, url) => {
+      if (err) return console.error(err)
+
+      console.log(url);
+      setQrCode(url);
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className=".wrapper">
+
+          <h1>QRCode Generator</h1>
+          <p>Enter your contract address here</p>
+
+          <form>
+            <input type="text" placeholder="Put contract address here!" value={url} onChange={(x) => setUrl(x.target.value)}></input>
+            <button onClick={GenerateQRCode}>Generate QR Code</button>
+          </form>
+
+          <img src={qrcode} />
+        </div>
+
     </div>
   );
 }
