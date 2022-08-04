f8155a2cfd46341fec950bbddf3e4d460f3547f7
Sina <sinarahimi.main@example.com>
2022-08-04 14:06:57 +0430

    Successful QRCode generation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import QRCode from 'qrcode';
+import QRCode from 'react-qr-code';
 
 function App() {
 
+  const [input, setInput] = useState(null);
   const [src, setSrc] = useState(null);
-  const [qrcode, setQRCode] = useState(null);
 
-  const GenerateQRCode = () => {
-    QRCode.toDataURL(src).then((data) => {
-      setQRCode(data);
-    })
+  let footer = "";
+  if (src) {
+    footer = <QRCode fgColor="blue" bgColor="red" size={300} value={src}/>
+  } else {
+    footer = "Nothing is generated yet";
   }
 
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSrc(input);
+  }
+  
   return (
-    <div className="App">
 
+    <div className="App">
         <div>
 
           <h1>QRCode Generator</h1>
           <p>Enter your contract address here</p>
 
-          <form>
-            <input type="text" placeholder="Put contract address here!" onChange={(x) => setSrc(x.target.value)}></input>
-            <button onClick={GenerateQRCode}>Generate QR Code</button>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Put contract address here!" onChange={(x) => setInput(x.target.value)}></input>
+            <input type="submit"></input>
           </form>
 
-        </div>
+        </div><br></br>
 
-        <img src={qrcode}/>
-
+        <div>{footer}</div>
     </div>
   );
 }
